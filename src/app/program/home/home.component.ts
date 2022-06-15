c6fac08e8288d4365040829a9f4c149f1ee0d17e
tsunglian <tsunglian@digiwin.com>
2022-06-15 16:21:50 +0800

    新增幻燈片功能main

--- src/app/program/home/home.component.ts
+++ src/app/program/home/home.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    x[1].style.display = "none";
+    carousel();
+  }
+  plusDivs(n: any): void  {   //手動換頁
+      this.showDivs(slideIndex += n);
+  }
+  showDivs(n: number) {
+    var i;
+    var x: any = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
   }
 
+  
+}
+
+var x: any = document.getElementsByClassName("mySlides");
+var slideIndex = 1;
+
+
+function carousel() {   //自動換頁
+  var i;
+  var x: any = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
